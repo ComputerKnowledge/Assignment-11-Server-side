f70d67f22f646003a3de2897f31b32e4d5d31cdb
Coumputer-knowledge <bbookself@gmail.com>
2025-03-13 13:04:40 +0600

    added api to fecth a single assignment

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
 // BASIC SERVER CONFIGURATION
-app.get("/", (req, res) => {
-  console.log("hello world");
-  res.send({ message: "Don't worry it fine to get error" });
-});
+// app.get("/", (req, res) => {
+//   console.log("hello world");
+//   res.send({ message: "Don't worry it fine to get error" });
+// });
 
 // DATABASE CONFIGURATION
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2zuqm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -29,8 +29,25 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const database1 = client.db("Assignment-11").collection("assignment");
-    app.get("/error", (req, res) => {
-      res.send("Don't worry it fine to get error");
+    // API to get all assignments
+    app.get("/assignments", async (req, res) => {
+      const result = await database1.find().toArray();
+      res.send(result);
+    });
+    // API to post a single assignment
+    app.post("/assignments", async (req, res) => {
+      const data = req.body;
+      const result = await database1.insertOne(data);
+      res.send(result);
+    });
+    // API to get a single assignment
+    app.get("/assignments/:id", async (req, res) => {
+      const { id } = req.params;
+      const query = {
+        _id: new ObjectId(id),
+      };
+      const result = await database1.findOne(query);
+      res.send(result);
     });
   } finally {
     // DO SOMETHING
