a24c9ef75e45e719c4c3c9e47f65b9b1e3fdc5fc
Coumputer-knowledge <bbookself@gmail.com>
2025-03-15 23:28:25 +0600

    install jst and cookie-parser

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 require("dotenv").config();
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const port = process.env.PORT || 5000;
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
 // BASIC SERVER CONFIGURATION
 // app.get("/", (req, res) => {
